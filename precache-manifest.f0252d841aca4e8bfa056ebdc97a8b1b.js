5e6193d2910c952dc775ffe57a3bb644e2b983cc
Vanessa Kang <vk@Vanessas-MBP.broadband>
2020-07-12 16:03:45 +0100

    Updates

---
+++ precache-manifest.f0252d841aca4e8bfa056ebdc97a8b1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54cab4e9f07241547c954afdba92b600",
+    "url": "/giphypedia/index.html"
+  },
+  {
+    "revision": "cb9371422d508fcd26a5",
+    "url": "/giphypedia/static/js/2.baab901d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giphypedia/static/js/2.baab901d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c791a294c689a5a772",
+    "url": "/giphypedia/static/js/main.31c853ce.chunk.js"
+  },
+  {
+    "revision": "39e1e5b92f4abee33c22",
+    "url": "/giphypedia/static/js/runtime-main.97d66612.js"
+  }
+]);
